29f4397e5f4084764e40f59531381fcb5f41a2b8
Atsushi <noisyheadcracker@gmail.com>
2019-03-27 19:27:59 +0900

    modified bullet core template

--- gdk-for-unity/workers/unity/Assets/Playground/Config/BulletTemplate.cs
+++ gdk-for-unity/workers/unity/Assets/Playground/Config/BulletTemplate.cs
@@ -12,22 +12,34 @@ namespace Playground
     {
         public static EntityTemplate CreateBulletEntityTemplate(Coordinates coords)
         {
-            var clientBullet = ClientBulletComponent.Component.CreateSchemaComponentData();
-            var workerBullet = WorkerBulletComponent.Component.CreateSchemaComponentData();
-            var launchable = Launchable.Component.CreateSchemaComponentData(new EntityId(0));
+            var template = new EntityTemplate();
+            template.AddComponent(new Position.Snapshot(coords), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Metadata.Snapshot("BulletCore"), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new ClientBullet.Snapshot(), WorkerUtils.UnityClient);
+            template.AddComponent(new WorkerBullet.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Launchable.SnapShot(), WorkerUtils.UnityGameLogic);
+            
+            template.SetReadAccess(WorkerUtils.AllWorkerAttributes.ToArray());
+            template.SetComponentWriterAccess(EntityAcl.ComponentId, WorkerUtils.UnityGameLogic);
 
-            var entityBuilder = EntityBuilder.Begin()
-                .AddPosition(coords.X, coords.Y, coords.Z, WorkerUtils.UnityGameLogic)
-                .AddMetadata("BulletCore", WorkerUtils.UnityGameLogic)
-                .SetPersistence(true)
-                .SetReadAcl(WorkerUtils.AllWorkerAttributes)
-                .AddComponent(clientBullet, WorkerUtils.UnityClient)
-                .AddComponent(workerBullet, WorkerUtils.UnityGameLogic)
-                .AddComponent(launchable, WorkerUtils.UnityGameLogic);
+            return template;
+            //var clientBullet = ClientBulletComponent.Component.CreateSchemaComponentData();
+            //var workerBullet = WorkerBulletComponent.Component.CreateSchemaComponentData();
+            //var launchable = Launchable.Component.CreateSchemaComponentData(new EntityId(0));
+            //
+            //var entityBuilder = EntityBuilder.Begin()
+            //    .AddPosition(coords.X, coords.Y, coords.Z, WorkerUtils.UnityGameLogic)
+            //    .AddMetadata("BulletCore", WorkerUtils.UnityGameLogic)
+            //    .SetPersistence(true)
+            //    .SetReadAcl(WorkerUtils.AllWorkerAttributes)
+            //    .AddComponent(clientBullet, WorkerUtils.UnityClient)
+            //    .AddComponent(workerBullet, WorkerUtils.UnityGameLogic)
+            //    .AddComponent(launchable, WorkerUtils.UnityGameLogic);
                 //.AddTransformSynchronizationComponents(WorkerUtils.UnityGameLogic,
                 //    location: coords.NarrowToUnityVector());
 
-            return entityBuilder.Build();
+            //return entityBuilder.Build();
         }
 
     }
