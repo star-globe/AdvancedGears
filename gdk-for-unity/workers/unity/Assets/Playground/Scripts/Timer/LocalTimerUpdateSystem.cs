e236857f8ac988ad9a5d658737c7886e0df93e7e
Atsushi <noisyheadcracker@gmail.com>
2019-07-12 19:20:57 +0900

    add localtimersystem

---
+++ gdk-for-unity/workers/unity/Assets/Playground/Scripts/Timer/LocalTimerUpdateSystem.cs
@@ -0,0 +1,31 @@
+using System;
+using Improbable;
+using Improbable.Gdk.Core;
+using Improbable.Gdk.ReactiveComponents;
+using Improbable.Gdk.Subscriptions;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace Playground
+{
+    [UpdateInGroup(typeof(FixedUpdateSystemGroup))]
+    internal class LocalTimerUpdateSystem : ComponentSystem
+    {
+        EntityQuery group;
+
+        private TimerInfo? timer;
+        public TimerInfo? Timer { get { return timer;} }
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+        }
+
+        protected override void OnUpdate()
+        {
+            // TODO Events
+        }
+    }
+}
