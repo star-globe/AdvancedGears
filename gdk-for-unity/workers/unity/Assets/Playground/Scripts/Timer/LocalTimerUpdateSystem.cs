943f8f858cc6cbabccae32301ef9562f9608cf04
Atsushi <noisyheadcracker@gmail.com>
2019-07-16 19:43:26 +0900

    modified

--- gdk-for-unity/workers/unity/Assets/Playground/Scripts/Timer/LocalTimerUpdateSystem.cs
+++ gdk-for-unity/workers/unity/Assets/Playground/Scripts/Timer/LocalTimerUpdateSystem.cs
@@ -15,17 +15,35 @@ namespace Playground
     {
         EntityQuery group;
 
+        CommandSystem commandSystem;
+
         private TimerInfo? timer;
         public TimerInfo? Timer { get { return timer;} }
 
+        bool connected = false;
         protected override void OnCreateManager()
         {
             base.OnCreateManager();
+
+            commandSystem = World.GetExistingSystem<CommandSystem>();
+
+            group = GetEntityQuery(
+                ComponentType.ReadOnly<OnConnected>(),
+                ComponentType.ReadOnly<WorkerEntityTag>()
+            );
         }
 
         protected override void OnUpdate()
         {
+            if (conntected)
+                return;
+
             // TODO Events
+            Entities.With(group).ForEach(entity =>
+            {
+                connected = true;
+                //commandSystem.SendCommand();
+            });
         }
     }
 }
