050f6228aa9cb3318b074ec3f366fcef5ea66823
Atsushi <noisyheadcracker@gmail.com>
2019-07-17 19:41:05 +0900

    changed localtimersystem

--- gdk-for-unity/workers/unity/Assets/Playground/Scripts/Timer/LocalTimerUpdateSystem.cs
+++ gdk-for-unity/workers/unity/Assets/Playground/Scripts/Timer/LocalTimerUpdateSystem.cs
@@ -13,42 +13,103 @@ namespace Playground
     [UpdateInGroup(typeof(FixedUpdateSystemGroup))]
     internal class LocalTimerUpdateSystem : ComponentSystem
     {
-        EntityQuery group;
-
+        WorkerSystem workerSystem;
         CommandSystem commandSystem;
+        private ILogDispatcher logDispatcher;
 
         private TimerInfo? timer;
         public TimerInfo? Timer { get { return timer;} }
 
-        bool connected = false;
+        private long? timerEntityQueryId;
+        private readonly List<EntityId> timerEntityIds = new List<EntityId>();
+        private readonly EntityQuery timerQuery = new EntityQuery
+        {
+            Constraint = new ComponentConstraint(WorldTimer.ComponentId),
+            ResultType = new SnapshotResultType()
+        };
+
+        private void SendTimerEntityQuery()
+        {
+            timerEntityQueryId = commandSystem.SendCommand(new WorldCommands.EntityQuery.Request
+            {
+                EntityQuery = timerQuery
+            });
+        }
+
         protected override void OnCreateManager()
         {
             base.OnCreateManager();
 
+            workerSystem = World.GetExistingSystem<WorkerSystem>();
             commandSystem = World.GetExistingSystem<CommandSystem>();
+            logDispatcher = workerSystem.LogDispatcher;
 
-            group = GetEntityQuery(
-                ComponentType.ReadOnly<OnConnected>(),
-                ComponentType.ReadOnly<WorkerEntityTag>()
-            );
-        }
-
-        protected override void OnUpdate()
-        {
-            if (connected)
-                return;
-
-            // TODO Events
-            Entities.With(group).ForEach(entity =>
-            {
-                connected = true;
-                //commandSystem.SendCommand();
-            });
+            SendTimerEntityQuery();
         }
 
         public void SetTimer(TimerInfo info)
         {
             timer = info;
+        }
+
+        protected override void OnUpdate()
+        {
+            if (timerEntityIds.Count > 0)
+            {
+                HandleSetTimer();
+            }
+            else
+            {
+                HandleEntityQueryResponses();
+            }
+        }
+
+        void HandleSetTimer()
+        {
+
+        }
+
+        int retries = 0;
+        void HandleEntityQueryResponses()
+        {
+            var entityQueryResponses = commandSystem.GetResponses<WorldCommands.EntityQuery.ReceivedResponse>();
+            for (var i = 0; i < entityQueryResponses.Count; i++)
+            {
+                ref readonly var response = ref entityQueryResponses[i];
+                if (response.RequestId != timerEntityQueryId)
+                {
+                    continue;
+                }
+
+                timerEntityQueryId = null;
+
+                if (response.StatusCode == StatusCode.Success)
+                {
+                    timerEntityIds.AddRange(response.Result.Keys);
+                }
+                else if (retries < 3)
+                {
+                    ++retries;
+
+                    logDispatcher.HandleLog(LogType.Warning, new LogEvent(
+                        $"Retrying timer query, attempt {retries}.\n{response.Message}"
+                    ));
+
+                    SendTimerEntityQuery();
+                }
+                else
+                {
+                    var retryText = retries == 0
+                        ? "1 attempt"
+                        : $"{retries + 1} attempts";
+
+                    logDispatcher.HandleLog(LogType.Error, new LogEvent(
+                        $"Unable to find timer after {retryText}."
+                    ));
+                }
+
+                break;
+            }
         }
     }
 }
