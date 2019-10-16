ba8d3fb5d8dbbd045c59112f3c74128a472d599e
star-globe <43998008+star-globe@users.noreply.github.com>
2019-10-17 03:50:34 +0900

    add system

--- root-project/workers/unity/Assets/Scripts/UI/MiniMap/MiniMapTest.cs
+++ root-project/workers/unity/Assets/Scripts/UI/MiniMap/MiniMapTest.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Improbable;
 using Improbable.Gdk.Core;
-using Improbable.Gdk.Core.Commands;
-using Improbable.Gdk.PlayerLifecycle;
-using Improbable.Worker.CInterop;
-using Improbable.Worker.CInterop.Query;
+using Improbable.Gdk.QueryBasedInterest;
 using ImprobableEntityQuery = Improbable.Worker.CInterop.Query.EntityQuery;
 using Unity.Entities;
 using UnityEngine;
@@ -22,9 +19,57 @@ namespace AdvancedGears
                 Constraint.All(
                     Constraint.Component<PlayerInfo.Component>(),
                     Constraint.RelativeSphere(20))
-                ).FilterResults(Positioin.ComponentId, PlayerInfo.ComponentId);
+                ).FilterResults(Position.ComponentId, PlayerInfo.ComponentId);
 
-            
+            var minimapQuery = InterestQuery.Query(
+                Constraint.All(
+                    Constraint.Component<MinimapRepresentaion.Component>(),
+                    Constraint.RelativeBox(50, double.PositiveInfinity, 50))
+                ).FilterResults(Position.ComponentId, MinimapRepresentaion.ComponentId);
+
+            var interestTemplate = InterestTemplate.Create()
+                .AddQueries<PlayerControls.Component>(playerQuery, minimapQuery);
+
+            var playerTemplate = new EntityTemplate();
+            playerTemplate.AddComponent(interestTemplate.ToSnapshot(), WorkerUtils.UnityGameLogic);
+        }
+    }
+
+    class MinimapSystem : SpatialComponentSystem
+    {
+        EntityQuery group;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            group = GetEntityQuery(
+                ComponentType.ReadOnly<Interest.Component>(),
+                ComponentType.ReadOnly<PlayerControls.Component>(),
+                ComponentType.ReadOnly<SpatialEntityId>()
+            );
+
+        }
+
+        protected override void OnUpdate()
+        {
+            Entities.With(group).ForEach((Entity entity,
+                                          ref Interest.Component interest,
+                                          ref PlayerControls.Component controls,
+                                          ref SpatialEntityId entityId) =>
+            {
+                var dic = interest.ComponentInterest;
+                foreach (var kvp in dic)
+                {
+                    var uid = kvp.Key;
+                    var value = kvp.Value;
+
+                    foreach(var q in value.Queries)
+                    {
+                        var list = q.ResultComponentId;
+                    }
+                }
+            });
         }
     }
 }
