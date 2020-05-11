0b75f69189c195c93a2e831255ce3f091a375673
Atsushi <noisyheadcracker@gmail.com>
2020-05-11 13:05:07 +0900

    add hex component

---
+++ root-project/workers/unity/Assets/Config/HexUtils.cs
@@ -0,0 +1,19 @@
+using Improbable.Gdk.Core;
+using Improbable.Gdk.GameObjectCreation;
+using Improbable.Gdk.PlayerLifecycle;
+using Improbable.Gdk.TransformSynchronization;
+using Unity.Entities;
+using System.Collections.Generic;
+using AdvancedGears.UI;
+
+namespace AdvancedGears
+{
+    public static class HexUtils
+    {
+        const float edge = 500.0f;
+        public static void SetHexCorners(Vector3 origin, int index, Vector3[] corners)
+        {
+
+        }
+    }
+}
