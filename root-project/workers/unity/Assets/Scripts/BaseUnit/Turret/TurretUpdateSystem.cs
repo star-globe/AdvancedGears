5a329f87e26c0b026d5b4e14e2a0ce0e48255e20
Atsushi <noisyheadcracker@gmail.com>
2020-06-18 17:54:44 +0900

    add turret

---
+++ root-project/workers/unity/Assets/Scripts/BaseUnit/Turret/TurretUpdateSystem.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Improbable;
+using Improbable.Gdk.Core;
+using Improbable.Gdk.Subscriptions;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace AdvancedGears
+{
+    [DisableAutoCreation]
+    [UpdateInGroup(typeof(FixedUpdateSystemGroup))]
+    internal class TurretUpdateSystem : BaseSearchSystem
+    {
+        private EntityQuery group;
+        IntervalChecker inter;
+
+        const int period = 2;
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            group = GetEntityQuery(
+                ComponentType.ReadWrite<TurretHub.Component>(),
+                ComponentType.ReadOnly<TurretHub.HasAuthority>(),
+                ComponentType.ReadOnly<BaseUnitStatus.Component>(),
+                ComponentType.ReadOnly<Transform>(),
+                ComponentType.ReadOnly<SpatialEntityId>()
+            );
+
+            inter = IntervalCheckerInitializer.InitializedChecker(period);
+        }
+
+        protected override void OnUpdate()
+        {
+            if (CheckTime(ref inter) == false)
+                return;
+
+            Entities.With(group).ForEach((Entity entity,
+                                          ref TurretHub.Component turret,
+                                          ref BaseUnitStatus.Component status,
+                                          ref SpatialEntityId entityId) =>
+            {
+                if (status.State != UnitState.Alive)
+                    return;
+
+                if (status.Type != UnitType.Stronghold)
+                    return;
+            });
+        }
+    }
+}
