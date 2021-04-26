1b1955f2be7b011e8d53f0afa6631a1302804657
Atsushi <noisyheadcracker@gmail.com>
2021-04-26 19:25:53 +0900

    add NaveMeshQueryTest

---
+++ root-project/workers/unity/Assets/Scripts/NavMeshQuery/NavMeshQueryTest.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.AI;
+using Unity.Collections;
+
+namespace AdvancedGears
+{
+    public class NavMeshQueryTest : MonoBehaviour
+    {
+        [SerializeField]
+        Transform start;
+
+        [SerializeField]
+        Transform end;
+
+        const int extents = 10;
+        const int maxPath = 32;
+        IEnumerator StartQuery()
+        {
+            NavMeshWorld world = NavMeshWorld.GetDefaultWorld();
+            NavMeshQuery query = new NavMeshQuery(world, Allocator.Persistent, maxPath);
+
+            NavMeshLocation startLocation = query.MapLocation(start.position, Vector3.up * extents, 0);
+            NavMeshLocation endLocation = query.MapLocation(end.positioin, Vector3.up * extents, 0);
+            PathQueryStatus status = query.BeginFindPath(startLocation, endLocatioin);
+
+            yield return new WaitWhile(() => {
+                status = query.UpdateFindPath(8, out int iterationsPerformed);
+                return status == PathQueryStatus.InProgress;
+            });
+
+            status = query.EndFindPath(out int pathsize);
+
+            NativeArray<PolygonId> path = new NativeArray<PolygonId>(pathsize, Allocator.Temp);
+            int pathResult = query.GetPathResult(path);
+            NativeArray<NavMeshLocation> pathStraight = new NativeArray<NavMeshLocation>(maxPath, Allocator.Temp);
+            NativeArray<StraightPathFlags> pathStreaigthFlag = new NativeArray<StraightPathFlags>(maxPath, Allocator.Temp);
+            NativeArray<float> vertexSize = new NativeArray<float>(maxPath, Allocator.Temp);
+
+            int straghtPathCount = 0;
+            query.Dispose();
+        }
+    }
+}
